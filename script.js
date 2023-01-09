64880f205c2a3ef9efc3957ef338004b9f0dd071
Court534 <courtney-stow@hotmail.co.uk>
2023-01-09 13:05:43 +0000

    Formatted code

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ async function getAPI() {
   const data = await response.json();
 
   document.getElementById("ft").textContent = JSON.stringify(data.full_total);
-  document.getElementById("ages").textContent = JSON.stringify((data.ages));
   document.getElementById("total_sold_brushes").textContent = JSON.stringify(data.total_sold_brushes);
   document.getElementById("type_total").textContent = JSON.stringify(data.type_total);
-  document.getElementById("delivery_status").textContent = JSON.stringify(data.delivery_status);
-  document.getElementById("dispatch_status").textContent = JSON.stringify(data.dispatch_status);
-  document.getElementById("orders_by_day").textContent = JSON.stringify(data.orders_by_day);
-  document.getElementById("orders_by_month").textContent = JSON.stringify(data.orders_by_month);
-  document.getElementById("orders_by_year").textContent = JSON.stringify(data.orders_by_year);
-  document.getElementById("bill_match_deliv").textContent = JSON.stringify(data.bill_match_deliv);
+  document.getElementById("top_postcodes").textContent = JSON.stringify(data.top_postcodes);
+  // document.getElementById("ages").textContent = JSON.stringify((data.ages));
+  // document.getElementById("delivery_status").textContent = JSON.stringify(data.delivery_status);
+  // document.getElementById("dispatch_status").textContent = JSON.stringify(data.dispatch_status);
+  // document.getElementById("orders_by_day").textContent = JSON.stringify(data.orders_by_day);
+  // document.getElementById("orders_by_month").textContent = JSON.stringify(data.orders_by_month);
+  // document.getElementById("orders_by_year").textContent = JSON.stringify(data.orders_by_year);
+  // document.getElementById("bill_match_deliv").textContent = JSON.stringify(data.bill_match_deliv);
   
   console.log(data);
-//   console.log(data.ages);
-//   console.log(data.top_postcodes);
-//   console.log(data.full_total);
+  console.log(data.ages);
+  console.log(data.top_postcodes);
+  console.log(data.full_total);
+  console.log(data.delivery_status);
 //   console.log(Object.keys(data));
 
 
