73db944508c925602e6b81f0a944dc5df6c97373
Court534 <courtney-stow@hotmail.co.uk>
2023-01-10 10:52:09 +0000

    Trying to fix errors

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const api_url = "https://jsfge6apa6.execute-api.us-east-1.amazonaws.com/api";
 async function getAPI() {
   const response = await fetch(api_url);
   const data = await response.json();
-
-  document.getElementById("ft").textContent = JSON.stringify(data.full_total);
+  
+  
+  var ft = document.getElementById("ft").textContent = JSON.stringify(data.full_total);
   document.getElementById("total_sold_brushes").textContent = JSON.stringify(data.total_sold_brushes);
   document.getElementById("type_total").textContent = JSON.stringify(data.type_total);
   document.getElementById("top_postcodes").textContent = JSON.stringify(data.top_postcodes);
@@ -16,12 +17,16 @@ async function getAPI() {
   // document.getElementById("orders_by_year").textContent = JSON.stringify(data.orders_by_year);
   // document.getElementById("bill_match_deliv").textContent = JSON.stringify(data.bill_match_deliv);
   
-  console.log(data.orders_by_month);
-  console.log(data.ages);
-  console.log(data.top_postcodes);
-  console.log(data.full_total);
-  console.log(data.delivery_status);
   console.log(data);
+  // console.log(data.orders_by_month);
+  // console.log(data.ages);
+  // console.log(data.top_postcodes);
+  // console.log(type_total.split(",")[0]);
+
+  // var type_total_ammount = type_total.split(",")[0].split(":")[1].replace(/"/g, '');
+  // .split(":")[1].replace(/"/g, ''));
+  // console.log(data.delivery_status);
+  // console.log(data);
 
 
 }
